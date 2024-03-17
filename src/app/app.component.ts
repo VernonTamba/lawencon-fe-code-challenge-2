406762dd7bb54368484ee618de9b9bea3cc742b7
VernonTamba <vernon.tamba777@gmail.com>
2024-03-17 16:25:36 +0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import videojs from 'video.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'fe-code-challenge-2-vernon-tamba';
+
+  isPlaying: boolean = false;
+
+  videoPlayer: any;
+
+  ngOnInit(): void {
+    this.videoPlayer = videojs('my-video');
+  }
+
+  onPlay(): void {
+    this.isPlaying = true;
+    this.videoPlayer.play();
+  }
+
+  onRestart(): void {
+    this.isPlaying = true;
+    this.videoPlayer.currentTime(0);
+    this.videoPlayer.play();
+  }
+
+  onPause(): void {
+    this.isPlaying = false;
+    this.videoPlayer.pause();
+  }
+
+  onFastForward(): void {
+    this.videoPlayer.currentTime(this.videoPlayer.currentTime() + 10);
+  }
+
+  onFastBackward(): void {
+    this.videoPlayer.currentTime(this.videoPlayer.currentTime() - 10);
+  }
 }
